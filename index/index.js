8532d20519bbe5a20d824df36282f340ad3456ce
LAPTOP-RP2R7J4M\77904 <779041463@qq.com>
2021-07-27 11:28:59 +0800

    首页排行榜样式调整

--- index/index.js
+++ index/index.js
@@ -25,58 +25,58 @@ $(function() {
     })
     
     // 热销商品轮播
-    new Swiper('.hot-swiper', {
-        pagination: '.hot-swiper .pagination',
-        paginationClickable: true,
-    });
+    // new Swiper('.hot-swiper', {
+    //     pagination: '.hot-swiper .pagination',
+    //     paginationClickable: true,
+    // });
 
     // // 新会员专区
-    var memversSwiper = new Swiper('.members-swiper', {
-        slidesPerView: 6,
-        loop: false
-    });
-    var memversProduct = $('.members-swiper .swiper-slide').size();
-    function memversSwiperArrowDisabled(){
-        var firstSwiperFlag = $('.members-swiper .swiper-slide').eq(0).hasClass('swiper-slide-visible');
-        var lastSwiperFlag = $('.members-swiper .swiper-slide').eq(memversProduct-1).hasClass('swiper-slide-visible');
-        if(firstSwiperFlag){
-            $('.main .members .bottom .swiper-button-prev').addClass('disabled')
-        }else{
-            $('.main .members .bottom .swiper-button-prev').removeClass('disabled')
-        }
-        if(lastSwiperFlag){
-            $('.main .members .bottom .swiper-button-next').addClass('disabled')
-        }else{
-            $('.main .members .bottom .swiper-button-next').removeClass('disabled')
-        }
-    }
-    memversSwiperArrowDisabled();
-    $('.members .swiper-button-prev').on('click', function(e){
-        e.preventDefault()
-        memversSwiper.swipePrev()
-        memversSwiperArrowDisabled()
-    })
-    $('.members .swiper-button-next').on('click', function(e){
-        e.preventDefault()
-        memversSwiper.swipeNext()
-        memversSwiperArrowDisabled()
-    })
+    // var memversSwiper = new Swiper('.members-swiper', {
+    //     slidesPerView: 6,
+    //     loop: false
+    // });
+    // var memversProduct = $('.members-swiper .swiper-slide').size();
+    // function memversSwiperArrowDisabled(){
+    //     var firstSwiperFlag = $('.members-swiper .swiper-slide').eq(0).hasClass('swiper-slide-visible');
+    //     var lastSwiperFlag = $('.members-swiper .swiper-slide').eq(memversProduct-1).hasClass('swiper-slide-visible');
+    //     if(firstSwiperFlag){
+    //         $('.main .members .bottom .swiper-button-prev').addClass('disabled')
+    //     }else{
+    //         $('.main .members .bottom .swiper-button-prev').removeClass('disabled')
+    //     }
+    //     if(lastSwiperFlag){
+    //         $('.main .members .bottom .swiper-button-next').addClass('disabled')
+    //     }else{
+    //         $('.main .members .bottom .swiper-button-next').removeClass('disabled')
+    //     }
+    // }
+    // memversSwiperArrowDisabled();
+    // $('.members .swiper-button-prev').on('click', function(e){
+    //     e.preventDefault()
+    //     memversSwiper.swipePrev()
+    //     memversSwiperArrowDisabled()
+    // })
+    // $('.members .swiper-button-next').on('click', function(e){
+    //     e.preventDefault()
+    //     memversSwiper.swipeNext()
+    //     memversSwiperArrowDisabled()
+    // })
 
 
 
 
-    // /**
-    //  * 楼层相关  
-    //  */ 
-    // // 排行榜
-    $(".floors .floor-item").each(function (index,el) { 
-        var elSwiper = '.floor'+(index+1)+' .rank-swiper';
-        var elPagination = '.floor'+(index+1)+' .pagination';
-        new Swiper(elSwiper, {
-            pagination: elPagination,
-            paginationClickable: true,
-        });
-    })
+    /**
+     * 楼层相关  
+    */ 
+    // 排行榜
+    // $(".floors .floor-item").each(function (index,el) { 
+    //     var elSwiper = '.floor'+(index+1)+' .rank-swiper';
+    //     var elPagination = '.floor'+(index+1)+' .pagination';
+    //     new Swiper(elSwiper, {
+    //         pagination: elPagination,
+    //         paginationClickable: true,
+    //     });
+    // })
 
     // 楼层选项卡product-categery点击
     $(".floors .product-categery ul.three-categery li").click(function () {
